79891b97888fa9c7057c3f0ac73b6ba2793504a5
Ignacio Aguilar <ignxcioxguilxr@gmail.com>
2025-10-03 15:46:04 -0300

    estructura de el menu

--- iu/main_menu.py
+++ iu/main_menu.py
@@ -3,3 +3,8 @@ from auxiliares.version import version_number
 
 def main_menu():
     print(f"{app_name} v{version_number}")
+    print("[1] Opción 1")
+    print("[2] Opción 2")
+    print("[3] Opción 3")
+    print("[4] Opción 4")
+    print("[5] Opción 5")
