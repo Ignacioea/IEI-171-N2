64baf90e95c817548731a99448b77268dc1bad50
Ignacio Aguilar <ignxcioxguilxr@gmail.com>
2025-10-23 20:20:07 -0300

    funcional el apartado de mostrar usuario

--- datos/conexion.py
+++ datos/conexion.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-DATABASE_URL = "mysql+mysqlconnector://root:@localhost:3306/iei_171_n2"
+DATABASE_URL = "mysql+mysqlconnector://root:@localhost:3306/biblioteca_system"
 motor_db = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=motor_db)
 
